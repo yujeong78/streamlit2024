bfb33717765154d7a5ae0a62ebf362fa132b2982
yujeong78 <yj7845@dsuyoung.ms.kr>
2024-07-24 11:51:20 +0900

    dice

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import random
 
-st.write('Hello World')
+st.header('주사위 게임', divider='rainbow')
 st.write('신유정')
+clicked=st.button('주사위던지기',type='primary')
+if clicked:
+    n=random.randit(1,6)
+    st.image(f'./img/{n}.png')
