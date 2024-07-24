b3fa242b26ac1346c2ad25badf4d7e6437481328
yujeong78 <yj7845@dsuyoung.ms.kr>
2024-07-24 11:55:03 +0900

    dice

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.header('주사위 게임', divider='rainbow')
 st.write('신유정')
 clicked=st.button('주사위던지기',type='primary')
 if clicked:
-    n=random.randit(1,6)
+    n=random.randint(1,6)
     st.image(f'./img/{n}.png')
